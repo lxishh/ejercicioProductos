ae39157cf4e72b4a14dbd828489182e0be471f34
lxishh <henrix913@gmail.com>
2024-09-03 17:09:30 -0400

    DjangoShop
    Todos los ajustes en el settings del proyecto realizados.
    
    Carga por defecto la tienda.
    
    logo agregado.

--- appProductos/views.py
+++ appProductos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def renderTienda(request):
+    return render(request, 'appTemplate/tienda.html')
+
+
+#Aquí irán las 3 páginas, todas con un request a la misma pagina
+#pero lo que cambia es la variable de data (Ahí ira la info correspondiente de cada una)
--- ejercicioProductos/urls.py
+++ ejercicioProductos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appProductos.views import renderTienda;
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', renderTienda)
 ]
